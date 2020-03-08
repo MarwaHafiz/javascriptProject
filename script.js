a93333854d0b4a2be5daeca0693c8b9da792dfb4
manarelsayed95 <manarelsayed969@gmail.com>
2020-03-08 22:41:19 +0200

    form validation

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-function validateform(){  
-    var name=document.myform.first-name.value;  
-    // var password=document.myform.password.value;  
-      
-    if (name==null || name==""){  
-      alert("Name can't be blank");  
-      return false;  
+function validateForm() {
+    var x = document.forms["myForm"]["first-name"].value;
+    var y = document.forms["myForm"]["last-name"].value;
+    if (x == "" || y=="") {
+      alert("Full Name must be filled out");
+      return false;
     }
-
-    if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-        if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            
-            return false;
-        }
-        else{
-            console.log("enter a valid email"); 
-        }
-    }
-    else {
-        if($(input).val().trim() == ''){
-            return false;
-        }
-    }
-}  
+  }
