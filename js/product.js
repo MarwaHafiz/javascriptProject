4affed2e599b9a9edf4fa7b5b02a0b5afeed6729
manarelsayed95 <manarelsayed969@gmail.com>
2020-03-16 19:23:10 +0200

    index product files

--- js/product.js
+++ js/product.js
@@ -1,286 +1,92 @@
-/* JS Document */
+const productContainer = document.querySelector("#productdetails");
+		
+const product = (Category, Name,Status ,Description,ProductPicUrl,Price,Quantity) => `
+<div id="product">
+<div class="container">
+	<div class="row details_row">
 
-/******************************
+		<!-- Product Image -->
+		<div class="col-lg-6">
+			<div class="details_image">
+				<div class="image"></div>
+				<img src="${ProductPicUrl}" alt="" class="image">
+				 <!-- <div class="details_image_large"><img src="images/details_1.jpg" alt=""></div> -->
+				<div class="details_image_thumbnails d-flex flex-row align-items-start justify-content-between">
+				</div>
+			</div>
+		</div>
+	
+		<!-- Product Content -->
+		<div class="col-lg-6">
+			<div class="details_content">
+					<div class="details_name">${Name}</div>
+				<div class="category">${Category}</div>
+			
+				<div class="details_price">$${Price}</div>
+				
 
-[Table of Contents]
+				<!-- In Stock -->
+				<div class="in_stock_container">
+					<div class="availability">Status:</div>
+					<span>${Status}</span>
+					<div>Quantity in stock: ${Quantity}</div>
+				
+				</div>
+				
+				<div class="details_text">
+					<p>${Description} </p>
+				</div>
 
-1. Vars and Inits
-2. Set Header
-3. Init Search
-4. Init Menu
-5. Init Image
-6. Init Quantity
-7. Init Isotope
+				<!-- Product Quantity -->
+				<div class="product_quantity_container">
+					<div class="product_quantity clearfix">
+						<span>Qty</span>
+						<input id="quantity_input" type="text" pattern="[0-9]*" value="1">
+						<div class="quantity_buttons">
+							<div id="quantity_inc_button" class="quantity_inc quantity_control"><i class="fa fa-chevron-up" aria-hidden="true"></i></div>
+							<div id="quantity_dec_button" class="quantity_dec quantity_control"><i class="fa fa-chevron-down" aria-hidden="true"></i></div>
+						</div>
+						
+					</div>
+					<div class="button cart_button"><a href="#" onclick="add()">Add to cart</a></div>
+				</div>
+
+				<!-- Share -->
+				<div class="details_share">
+					<span>Share:</span>
+					<ul>
+						<li><a href="https://www.pinterest.com"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+						<li><a href="https://www.instagram.com"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+						<li><a href="https://www.facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li><a href="https://www.twitter.com"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+				
+			</div>
+		</div>
+		
+	</div> </br> </br>`;
+(async function loadApi() {
+const urlParams= new URLSearchParams(window.location.search);
+var ProductId= urlParams.get('id')
+const req = await fetch("https://afternoon-falls-30227.herokuapp.com/api/v1/products/"+ProductId);
+const resp = await req.json();
+const detail=resp.data;
+
+let htmlString = "";
+
+htmlString += product(detail.Category, detail.Name,detail.Status,detail.Description, detail.ProductPicUrl,detail.Price,detail.Quantity);
 
 
-******************************/
+productContainer.innerHTML = htmlString;
+})();
 
-$(document).ready(function()
-{
-	"use strict";
+var itemArr = []
+itemArr=  JSON.parse(localStorage.getItem("id"))
 
-	/* 
-
-	1. Vars and Inits
-
-	*/
-
-	var header = $('.header');
-	var hambActive = false;
-	var menuActive = false;
-
-	setHeader();
-
-	$(window).on('resize', function()
-	{
-		setHeader();
-	});
-
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
-
-	initSearch();
-	initMenu();
-	initImage();
-	initQuantity();
-	initIsotope();
-
-	/* 
-
-	2. Set Header
-
-	*/
-
-	function setHeader()
-	{
-		if($(window).scrollTop() > 100)
-		{
-			header.addClass('scrolled');
-		}
-		else
-		{
-			header.removeClass('scrolled');
-		}
-	}
-
-	/* 
-
-	3. Init Search
-
-	*/
-
-	function initSearch()
-	{
-		if($('.search').length && $('.search_panel').length)
-		{
-			var search = $('.search');
-			var panel = $('.search_panel');
-
-			search.on('click', function()
-			{
-				panel.toggleClass('active');
-			});
-		}
-	}
-
-	/* 
-
-	4. Init Menu
-
-	*/
-
-	function initMenu()
-	{
-		if($('.hamburger').length)
-		{
-			var hamb = $('.hamburger');
-
-			hamb.on('click', function(event)
-			{
-				event.stopPropagation();
-
-				if(!menuActive)
-				{
-					openMenu();
-					
-					$(document).one('click', function cls(e)
-					{
-						if($(e.target).hasClass('menu_mm'))
-						{
-							$(document).one('click', cls);
-						}
-						else
-						{
-							closeMenu();
-						}
-					});
-				}
-				else
-				{
-					$('.menu').removeClass('active');
-					menuActive = false;
-				}
-			});
-
-			//Handle page menu
-			if($('.page_menu_item').length)
-			{
-				var items = $('.page_menu_item');
-				items.each(function()
-				{
-					var item = $(this);
-
-					item.on('click', function(evt)
-					{
-						if(item.hasClass('has-children'))
-						{
-							evt.preventDefault();
-							evt.stopPropagation();
-							var subItem = item.find('> ul');
-						    if(subItem.hasClass('active'))
-						    {
-						    	subItem.toggleClass('active');
-								TweenMax.to(subItem, 0.3, {height:0});
-						    }
-						    else
-						    {
-						    	subItem.toggleClass('active');
-						    	TweenMax.set(subItem, {height:"auto"});
-								TweenMax.from(subItem, 0.3, {height:0});
-						    }
-						}
-						else
-						{
-							evt.stopPropagation();
-						}
-					});
-				});
-			}
-		}
-	}
-
-	function openMenu()
-	{
-		var fs = $('.menu');
-		fs.addClass('active');
-		hambActive = true;
-		menuActive = true;
-	}
-
-	function closeMenu()
-	{
-		var fs = $('.menu');
-		fs.removeClass('active');
-		hambActive = false;
-		menuActive = false;
-	}
-
-	/* 
-
-	5. Init Image
-
-	*/
-
-	function initImage()
-	{
-		var images = $('.details_image_thumbnail');
-		var selected = $('.details_image_large img');
-
-		images.each(function()
-		{
-			var image = $(this);
-			image.on('click', function()
-			{
-				var imagePath = new String(image.data('image'));
-				selected.attr('src', imagePath);
-				images.removeClass('active');
-				image.addClass('active');
-			});
-		});
-	}
-
-	/* 
-
-	6. Init Quantity
-
-	*/
-
-	function initQuantity()
-	{
-		// Handle product quantity input
-		if($('.product_quantity').length)
-		{
-			var input = $('#quantity_input');
-			var incButton = $('#quantity_inc_button');
-			var decButton = $('#quantity_dec_button');
-
-			var originalVal;
-			var endVal;
-
-			incButton.on('click', function()
-			{
-				originalVal = input.val();
-				endVal = parseFloat(originalVal) + 1;
-				input.val(endVal);
-			});
-
-			decButton.on('click', function()
-			{
-				originalVal = input.val();
-				if(originalVal > 0)
-				{
-					endVal = parseFloat(originalVal) - 1;
-					input.val(endVal);
-				}
-			});
-		}
-	}
-
-	/* 
-
-	7. Init Isotope
-
-	*/
-
-	function initIsotope()
-	{
-		var sortingButtons = $('.product_sorting_btn');
-		var sortNums = $('.num_sorting_btn');
-
-		if($('.product_grid').length)
-		{
-			var grid = $('.product_grid').isotope({
-				itemSelector: '.product',
-				layoutMode: 'fitRows',
-				fitRows:
-				{
-					gutter: 30
-				},
-	            getSortData:
-	            {
-	            	price: function(itemElement)
-	            	{
-	            		var priceEle = $(itemElement).find('.product_price').text().replace( '$', '' );
-	            		return parseFloat(priceEle);
-	            	},
-	            	name: '.product_name',
-	            	stars: function(itemElement)
-	            	{
-	            		var starsEle = $(itemElement).find('.rating');
-	            		var stars = starsEle.attr("data-rating");
-	            		return stars;
-	            	}
-	            },
-	            animationOptions:
-	            {
-	                duration: 750,
-	                easing: 'linear',
-	                queue: false
-	            }
-	        });
-		}
-	}
-
-});
+function add() {
+    itemArr.push(ProductId)
+    var id = window.localStorage.setItem("id",JSON.stringify(itemArr))
+   
+    window.location.href = "cart.html"
+}
